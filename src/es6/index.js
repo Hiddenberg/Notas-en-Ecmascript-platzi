86fb1f8311391481413c4804e9aa66d05eeb3443
Victor Pazaran <victor_hals@hotmail.com>
2020-09-07 15:02:48 -0500

    clase 3 -Arrow Functions, Promesas y Parametros en objetos

--- src/es6/index.js
+++ src/es6/index.js
@@ -31,7 +31,7 @@ console.log(epicPhrase2); // "hello world"
 
 
 
-/* -------------------- clase 2 -strings en multilinea -------------------- */
+/* ------------------------- clase 2 -strings en multilinea ------------------------- */
 
 /* Para hacer strings con multiples lineas normalmente lo que se hacia era esto */
 let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod placerat est ac semper \n"
@@ -106,3 +106,75 @@ const a = 'b';
 //Por lo cual, si tratamos de reasignarla de la siguiente forma, el programa nos dará un error
 a = 'a'; //Error
 
+
+
+/* ------------------------- clase 3 -Arrow Functions, Promesas y Parametros en objetos ------------------------- */
+
+/* ++Creacion de objetos y sus propiedades */
+/* Anteriormente para crear un objeto y sus propiedades se hacia de la siguiente forma */
+let nombre = 'victor';
+let edad = 24;
+
+obj = { name: nombre, age: edad };
+
+
+/* Y la nueva forma de hacerlo con ecmascript 6 es de la siguiente */
+
+obj2 = { nombre, edad }; // De esta forma el objeto automaticamente va a recibir tanto la variable como el valor de la misma
+
+/* por lo que */
+
+console.log(obj2.nombre) // nos devolvera 'victor'
+console.log(obj2.edad) // nos devolvera '24'
+
+
+
+/* ++Arrow functions */
+// Estas nos permiten tener una sintaxis mas reducida, y al mismo tiempo un 'this' no vinculable
+
+const names = [
+   {name: 'oscar', age: 32},
+   {name: 'yesica', age: 27}
+];
+
+/* La forma tradicional de ejecutar un map, seria pasandole una funcion anonima como parametro de la siguiente forma */
+let listOfNames = names.map(function(item) {
+   console.log(item.name)
+})
+
+
+/* Cabe destacar que las "arrow functions" siempre son funciones anonimas */
+/* Ahora con ecmascript6 y las arrow functions podemos hacer el map de la siguiente forma */
+let listOfNames2 = names.map(item => console.log(item));
+
+/* Tambien es posible asignar una arrow function a una variable para posteriormente pode usarla como una funcion normal */
+const listOfNames3 = (name, age, country) => {
+   // Code
+}
+
+/* ↓ Si la funcion solo tendra un solo parametro podemos pasarlo directamente si necesidad de usar los parentesis */
+const listOfNames4 = name => {
+   // Code
+}
+
+/* ↓ De igual forma si solo vamos a ejecutar y/o retornar una sola cosa, tambien podemos pasarla directametne sin los brackets */
+const cumpleaños = edad => edad + 1;
+
+
+/* ++Promesas */
+/* Las pronmesas son realmente utiles en JS ya que se encargan de manejar peticiones que pueden (O NO) ser cumplidas en algun momento */
+
+const helloPromise = () => {
+   return new Promise((resolve, reject) => {
+      if(false) {
+         resolve('hey!')
+      } else {
+         reject('ups!, algo salio mal')
+      }
+   })
+}
+
+helloPromise()
+   .then(response => console.log(response))
+   .then(() => console.log('hola'))
+   .catch(error => console.log(error))
